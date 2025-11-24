8eab90e55e786619694613972ef3c34cdd07dba9
SivaSai <sivasaip86@gmail.com>
2025-11-24 20:40:28 +0530

    vendor routes final fix

--- routes/vendorRoutes.js
+++ routes/vendorRoutes.js
@@ -15,10 +15,4 @@ router.get("/all-vendors", vendorController.getAllVendors);
 // Get single vendor by ID
 router.get("/single-vendor/:id", vendorController.getVendorById);
 
-// Update vendor (optional — if you want update feature)
-router.put("/update/:id", vendorController.updateVendor);
-
-// Delete vendor (optional — if you want delete feature)
-router.delete("/delete/:id", vendorController.deleteVendor);
-
 module.exports = router;
